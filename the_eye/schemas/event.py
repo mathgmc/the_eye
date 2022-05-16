6a0b04cec50334cbf6a51cdf2f7082ffcc338edb
Math Carvalho <matheusgmc@hotmail.com>
2022-05-16 08:09:37 -0300

    Adding schemas module, and adding post event schemas

---
+++ the_eye/schemas/event.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from enum import Enum
+from pydantic import BaseModel, UUID4
+from pydantic.class_validators import validator
+
+
+class EventCategory(Enum):
+    PAGE_INTERACTION = "page interaction"
+    FORM_INTERACTION = "form interaction"
+
+
+class PostEventSchema(BaseModel):
+    session_id: str
+    category: EventCategory
+    name: str
+    data: dict
+    timestamp: datetime
+
+    @validator("timestamp")
+    def timestamp_validator(cls, value):
+        if value > datetime.utcnow():
+            raise ValueError("Invalid timestamp")
+        return value
+
+    @validator("session_id")
+    def session_id_validator(cls, value):
+        UUID4(value)
+        return value
