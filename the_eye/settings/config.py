770148cf03879883788660d34a6d36f3092e9ea7
Math Carvalho <matheusgmc@hotmail.com>
2022-05-16 23:04:54 -0300

    Adding logging config

--- the_eye/settings/config.py
+++ the_eye/settings/config.py
@@ -1,20 +1,32 @@
 import os
+import logging
+import sys
 
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+# APP
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
-#DB Config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
-    os.getenv('DB_USER', 'the_eye_app'),
-    os.getenv('DB_PASSWORD', 'theeye123'),
-    os.getenv('DB_HOST', 'db'),
-    os.getenv('DB_NAME', 'eye')
+# DB Config
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://{}:{}@{}/{}".format(
+    os.getenv("DB_USER", "the_eye_app"),
+    os.getenv("DB_PASSWORD", "theeye123"),
+    os.getenv("DB_HOST", "db"),
+    os.getenv("DB_NAME", "eye"),
 )
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# LOGGING
+logFormatter = logging.Formatter(
+    "%(asctime)s [%(threadName)s] [%(levelname)s]  %(message)s"
+)
+rootLogger = logging.getLogger()
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
